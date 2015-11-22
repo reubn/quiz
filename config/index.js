72ff1d31dc70374bd6ed9af997b02eaf3e9780ed
Reuben <reuben.eggar@me.com>
2015-11-22 11:04:51 +0000

    Back Config

---
+++ config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "app": {
+    "name": "Quiz",
+    "ip": null,
+    "port": 80,
+    "environment": "development",
+    "https": false,
+    "statusColors": ["", "", "green", "cyan", "yellow", "red"]
+  }
+}
