55f8c4c3b82620434b4a1f67a67ecbb1f45913e3
Reuben <reuben.eggar@me.com>
2015-11-22 13:27:22 +0000

    Removed URL Hash

--- front/js/routes.js
+++ front/js/routes.js
@@ -4,12 +4,14 @@ const Router = require("react-router").Router
 const Route = require("react-router").Route
 const IndexRoute = require("react-router").IndexRoute
 
+const History = require("History")
+
 const Display = require("Display")
 const SubjectsView = require("SubjectsView")
 const Subject = require("Subject")
 
 module.exports = function(parent){
-  return (<Router>
+  return (<Router history={History.createHashHistory({queryKey: false})}>
             <Route path="/" component={Display}>
               <IndexRoute component={SubjectsView} freezer={parent.freezer}/>
               <Route path="subjects" component={SubjectsView} freezer={parent.freezer}/>
